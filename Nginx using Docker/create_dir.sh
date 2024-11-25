81966765740a77bb592ce33f329cf346fa071d54
Dmitry Santalov <dmitsantalov@yandex.ru>
2024-11-25 09:14:21 +0300

    Create create_dir.sh

---
+++ Nginx using Docker/create_dir.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# Создание основной директории
+mkdir -p serverspace
+cd serverspace
+
+# Создание внутренних директорий и конфигурационных файлов
+mkdir -p project/conf-fi
+mkdir -p html/files
+touch project/conf-files
+touch project/nginx.conf
+touch html/files/servers
+
