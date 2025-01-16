6128cb516a207d371e647df4a706e70344893b19
Dmitry Santalov <dmitsantalov@yandex.ru>
2025-01-16 15:29:43 +0300

    Create (step_4)up_repository_docker.sh

---
+++ Install Docker on ubuntu/(step_4)up_repository_docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu jammy stable"| sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
