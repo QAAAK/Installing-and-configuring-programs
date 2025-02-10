297238afac87ccd5e43599ee2bb115d6962cda2d
Dmitry Santalov <dmitsantalov@yandex.ru>
2025-02-10 15:11:17 +0300

    Update and rename (step_2)install_repo.sh to (step_2)install_repo_and_gitlab.sh

--- Install Gitlab on Ubuntu/(step_2)install_repo_and_gitlab.sh
+++ Install Gitlab on Ubuntu/(step_2)install_repo_and_gitlab.sh
@@ -3,3 +3,5 @@
 apt install curl openssh-server ca-certificates;
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash; # community version
+
+apt install gitlab-ce;
