fae63dc3cab071467cb0e291cdddb0f9168d2268
Dmitry Santalov <dmitsantalov@yandex.ru>
2025-02-10 15:09:57 +0300

    Create (step_2)install_repo.sh

---
+++ Install Gitlab on Ubuntu/(step_2)install_repo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+apt install curl openssh-server ca-certificates;
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh | sudo bash; # community version
